0e808164aa941844c70f90260256ff5c9b790934
mika <mika@localhost>
2013-03-19 09:46:54 +0200

    initial revision

---
+++ src/main/java/alt/rtps/message/CDREncapsulation.java
@@ -0,0 +1,27 @@
+package alt.rtps.message;
+
+import alt.rtps.transport.RTPSByteBuffer;
+
+public class CDREncapsulation extends DataEncapsulation {
+
+	private final RTPSByteBuffer bb;
+	private final short options;
+
+
+	public CDREncapsulation(RTPSByteBuffer bb, short options) {
+		this.bb = bb;
+		this.options = options;
+	}
+	
+
+	@Override
+	public boolean containsData() {
+		return true;
+	}
+
+	@Override
+	public byte[] getSerializedPayload() {
+		return null;
+	}
+
+}
