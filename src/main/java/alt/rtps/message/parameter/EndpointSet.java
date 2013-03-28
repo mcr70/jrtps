b90cca1e6f32e83375d5754168b0b4fce83570d4
mika <mika@localhost>
2013-03-28 14:05:40 +0200

    BuiltinEndpointSet constants are stored in one place: alt.rtps.types.BuiltinEndpointSet_t

--- src/main/java/alt/rtps/message/parameter/EndpointSet.java
+++ src/main/java/alt/rtps/message/parameter/EndpointSet.java
@@ -1,22 +1,16 @@
 package alt.rtps.message.parameter;
 
 import alt.rtps.transport.RTPSByteBuffer;
+import alt.rtps.types.BuiltinEndpointSet_t;
 
+/**
+ * This Parameter identifies the kinds of built-in SEDP endpoints that are available in the Participant.
+ * 
+ * @author mika.riekkinen
+ *
+ */
 public class EndpointSet extends Parameter {
 	private int endpointSet;
-	
-	public static final int DISC_BUILTIN_ENDPOINT_PARTICIPANT_ANNOUNCER = 0x00000001 << 0;
-	public static final int DISC_BUILTIN_ENDPOINT_PARTICIPANT_DETECTOR = 0x00000001 << 1;
-	public static final int DISC_BUILTIN_ENDPOINT_PUBLICATION_ANNOUNCER = 0x00000001 << 2;
-	public static final int DISC_BUILTIN_ENDPOINT_PUBLICATION_DETECTOR = 0x00000001 << 3;
-	public static final int DISC_BUILTIN_ENDPOINT_SUBSCRIPTION_ANNOUNCER = 0x00000001 << 4;
-	public static final int DISC_BUILTIN_ENDPOINT_SUBSCRIPTION_DETECTOR = 0x00000001 << 5;
-	public static final int DISC_BUILTIN_ENDPOINT_PARTICIPANT_PROXY_ANNOUNCER = 0x00000001 << 6;
-	public static final int DISC_BUILTIN_ENDPOINT_PARTICIPANT_PROXY_DETECTOR = 0x00000001 << 7;
-	public static final int DISC_BUILTIN_ENDPOINT_PARTICIPANT_STATE_ANNOUNCER = 0x00000001 << 8;
-	public static final int DISC_BUILTIN_ENDPOINT_PARTICIPANT_STATE_DETECTOR = 0x00000001 << 9;
-	public static final int BUILTIN_ENDPOINT_PARTICIPANT_MESSAGE_DATA_WRITER = 0x00000001 << 10;
-	public static final int BUILTIN_ENDPOINT_PARTICIPANT_MESSAGE_DATA_READER = 0x00000001 << 11;
 	
 	protected EndpointSet(ParameterEnum e, int endpoints) {
 		super(e);
@@ -25,51 +19,51 @@ public class EndpointSet extends Parameter {
 	}
 	
 	public boolean hasParticipantAnnouncer() {
-		return (endpointSet &  DISC_BUILTIN_ENDPOINT_PARTICIPANT_ANNOUNCER) != 0;
+		return (endpointSet &  BuiltinEndpointSet_t.DISC_BUILTIN_ENDPOINT_PARTICIPANT_ANNOUNCER) != 0;
 	}
 	
 	public boolean hasParticipantDetector() {
-		return (endpointSet &  DISC_BUILTIN_ENDPOINT_PARTICIPANT_DETECTOR) != 0;
+		return (endpointSet &  BuiltinEndpointSet_t.DISC_BUILTIN_ENDPOINT_PARTICIPANT_DETECTOR) != 0;
 	}
 
 	public boolean hasPublicationAnnouncer() {
-		return (endpointSet &  DISC_BUILTIN_ENDPOINT_PUBLICATION_ANNOUNCER) != 0;
+		return (endpointSet &  BuiltinEndpointSet_t.DISC_BUILTIN_ENDPOINT_PUBLICATION_ANNOUNCER) != 0;
 	}
 	
 	public boolean hasPublicationDetector() {
-		return (endpointSet &  DISC_BUILTIN_ENDPOINT_PUBLICATION_DETECTOR) != 0;
+		return (endpointSet &  BuiltinEndpointSet_t.DISC_BUILTIN_ENDPOINT_PUBLICATION_DETECTOR) != 0;
 	}
 
 	public boolean hasSubscriptionAnnouncer() {
-		return (endpointSet &  DISC_BUILTIN_ENDPOINT_SUBSCRIPTION_ANNOUNCER) != 0;
+		return (endpointSet &  BuiltinEndpointSet_t.DISC_BUILTIN_ENDPOINT_SUBSCRIPTION_ANNOUNCER) != 0;
 	}
 	
 	public boolean hasSubscriptionDetector() {
-		return (endpointSet &  DISC_BUILTIN_ENDPOINT_SUBSCRIPTION_DETECTOR) != 0;
+		return (endpointSet &  BuiltinEndpointSet_t.DISC_BUILTIN_ENDPOINT_SUBSCRIPTION_DETECTOR) != 0;
 	}
 
 	public boolean hasParticipantProxyAnnouncer() {
-		return (endpointSet &  DISC_BUILTIN_ENDPOINT_PARTICIPANT_PROXY_ANNOUNCER) != 0;
+		return (endpointSet &  BuiltinEndpointSet_t.DISC_BUILTIN_ENDPOINT_PARTICIPANT_PROXY_ANNOUNCER) != 0;
 	}
 	
 	public boolean hasParticipantProxyDetector() {
-		return (endpointSet &  DISC_BUILTIN_ENDPOINT_PARTICIPANT_PROXY_DETECTOR) != 0;
+		return (endpointSet &  BuiltinEndpointSet_t.DISC_BUILTIN_ENDPOINT_PARTICIPANT_PROXY_DETECTOR) != 0;
 	}
 
 	public boolean hasParticipantStateAnnouncer() {
-		return (endpointSet &  DISC_BUILTIN_ENDPOINT_PARTICIPANT_STATE_ANNOUNCER) != 0;
+		return (endpointSet &  BuiltinEndpointSet_t.DISC_BUILTIN_ENDPOINT_PARTICIPANT_STATE_ANNOUNCER) != 0;
 	}
 	
 	public boolean hasParticipantStateDetector() {
-		return (endpointSet &  DISC_BUILTIN_ENDPOINT_PARTICIPANT_STATE_DETECTOR) != 0;
+		return (endpointSet &  BuiltinEndpointSet_t.DISC_BUILTIN_ENDPOINT_PARTICIPANT_STATE_DETECTOR) != 0;
 	}
 	
 	public boolean hasParticipantMessageDataReader() {
-		return (endpointSet &  BUILTIN_ENDPOINT_PARTICIPANT_MESSAGE_DATA_READER) != 0;
+		return (endpointSet &  BuiltinEndpointSet_t.BUILTIN_ENDPOINT_PARTICIPANT_MESSAGE_DATA_READER) != 0;
 	}
 	
 	public boolean hasParticipantMessageDataWriter() {
-		return (endpointSet &  BUILTIN_ENDPOINT_PARTICIPANT_MESSAGE_DATA_WRITER) != 0;
+		return (endpointSet &  BuiltinEndpointSet_t.BUILTIN_ENDPOINT_PARTICIPANT_MESSAGE_DATA_WRITER) != 0;
 	}
 
 	/**
