f5883b6df60d8dc2e1fd057a900548dbc3645324
Mika Riekkinen <mika.riekkinen@gmail.com>
2013-04-17 08:19:33 +0300

    Added generics

--- src/main/java/alt/rtps/DataListener.java
+++ src/main/java/alt/rtps/DataListener.java
@@ -2,6 +2,6 @@ package alt.rtps;
 
 import alt.rtps.types.Time_t;
 
-public interface DataListener {
-	public void onData(Object data, Time_t timestamp);
+public interface DataListener<T> {
+	public void onData(T data, Time_t timestamp);
 }
