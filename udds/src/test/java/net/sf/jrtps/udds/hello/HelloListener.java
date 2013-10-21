c5f9165b71653bb4a1a894e4ac1d8a49649897d6
mika <mika@localhost>
2013-10-21 19:48:53 +0300

    HelloListener

---
+++ udds/src/test/java/net/sf/jrtps/udds/hello/HelloListener.java
@@ -0,0 +1,13 @@
+package net.sf.jrtps.udds.hello;
+
+import java.util.List;
+
+import net.sf.jrtps.udds.DataListener;
+
+public class HelloListener implements DataListener<HelloMessage> {
+
+	@Override
+	public void onDataAvailable(List<HelloMessage> samples) {		
+		System.out.println("onDataAvailable(): " + samples);
+	}
+}
