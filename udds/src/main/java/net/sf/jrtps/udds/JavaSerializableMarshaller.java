5cea9400158947de8d796ec0d687980e2a712baa
mcr70 <mcr70@localhost>
2013-11-04 12:39:22 +0200

    Added support to register type specific Marshallers

--- udds/src/main/java/net/sf/jrtps/udds/JavaSerializableMarshaller.java
+++ udds/src/main/java/net/sf/jrtps/udds/JavaSerializableMarshaller.java
@@ -4,6 +4,7 @@ import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
+import java.io.Serializable;
 import java.lang.reflect.Field;
 
 import net.sf.jrtps.Marshaller;
@@ -14,19 +15,31 @@ import net.sf.jrtps.transport.RTPSByteBuffer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-class JavaSerializableMarshaller extends Marshaller<Object> {
+/**
+ * A default Marshaller used by udds.
+ * This Marshaller marshalls objects by writing them to <i>ObjectOutputStream</i> and reading from
+ * <i>ObjectInputStream</i>. JavaSerializableMarshaller supports net.sf.jrtps.Key annotation
+ * to form a Key for the marshalled Object.
+ * 
+ * @see java.io.ObjectOutputStream
+ * @see java.io.ObjectInputStream
+ * @see net.sf.jrtps.Key
+ * 
+ * @author mcr70
+ *
+ */
+public class JavaSerializableMarshaller extends Marshaller<Serializable> {
 	private static final Logger log = LoggerFactory.getLogger(JavaSerializableMarshaller.class);
 
 	@Override
-	public byte[] extractKey(Object data) {
+	public byte[] extractKey(Serializable data) {
 		try {
 			ByteArrayOutputStream baos = new ByteArrayOutputStream(1024);
 			ObjectOutputStream oos = new ObjectOutputStream(baos);
-			RTPSByteBuffer bb = new RTPSByteBuffer(new byte[1024]);
+
 			for (Field f : keyFields) {
 				f.setAccessible(true);
 				Object value = f.get(data);
-				System.out.println("*** Writing key: " + value);
 				oos.writeObject(value);
 			}
 			
@@ -40,7 +53,7 @@ class JavaSerializableMarshaller extends Marshaller<Object> {
 	}
 
 	@Override
-	public Object unmarshall(DataEncapsulation dEnc) throws IOException {
+	public Serializable unmarshall(DataEncapsulation dEnc) throws IOException {
 		CDREncapsulation cdrEnc = (CDREncapsulation) dEnc;		
 		RTPSByteBuffer bb = cdrEnc.getBuffer();
 
@@ -52,11 +65,11 @@ class JavaSerializableMarshaller extends Marshaller<Object> {
 		} catch (ClassNotFoundException e) {
 			throw new IOException(e);
 		}
-		return o;
+		return (Serializable) o;
 	}
 
 	@Override
-	public DataEncapsulation marshall(Object data) throws IOException {
+	public DataEncapsulation marshall(Serializable data) throws IOException {
 		CDREncapsulation cdrEnc = new CDREncapsulation(1024); // TODO: hardcoded
 		RTPSByteBuffer bb = cdrEnc.getBuffer();
 
