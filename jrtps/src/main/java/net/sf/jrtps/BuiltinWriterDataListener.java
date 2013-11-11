25fcd82c9e97f6454bd2aaa35d133fc2f1f58c20
Mika Riekkinen <mika.riekkinen@gmail.com>
2013-11-11 09:17:29 +0200

    Removed DataListener in favor of SampleListener

--- jrtps/src/main/java/net/sf/jrtps/BuiltinWriterDataListener.java
+++ jrtps/src/main/java/net/sf/jrtps/BuiltinWriterDataListener.java
@@ -1,16 +1,15 @@
 package net.sf.jrtps;
 
 import java.util.HashMap;
+import java.util.List;
 
 import net.sf.jrtps.builtin.WriterData;
-import net.sf.jrtps.message.parameter.StatusInfo;
 import net.sf.jrtps.types.GUID_t;
-import net.sf.jrtps.types.Time_t;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-class BuiltinWriterDataListener implements DataListener<WriterData>{
+class BuiltinWriterDataListener implements SampleListener<WriterData>{
 	private static final Logger log = LoggerFactory.getLogger(BuiltinWriterDataListener.class);
 
 	private final RTPSParticipant participant;
@@ -20,21 +19,25 @@ class BuiltinWriterDataListener implements DataListener<WriterData>{
 		this.participant = p;
 		this.discoveredWriters = discoveredWriters;
 	}
-	
-	@Override
-	public void onData(WriterData writerData, Time_t timestamp, StatusInfo sInfo) {
-		GUID_t key = writerData.getKey();
-		if (discoveredWriters.put(key, writerData) == null) {
-			log.debug("Discovered a new writer {} for topic {}, type {}", key, writerData.getTopicName(), writerData.getTypeName());
-		}
 
-		RTPSReader r = participant.getReaderForTopic(writerData.getTopicName());
-		if (r != null) {
-			if (sInfo.isDisposed()) {
-				r.removeMatchedWriter(writerData);
+	@Override
+	public void onSamples(List<Sample<WriterData>> samples) {
+		for (Sample<WriterData> wdSample : samples) {
+			WriterData writerData = wdSample.getData();
+
+			GUID_t key = writerData.getKey();
+			if (discoveredWriters.put(key, writerData) == null) {
+				log.debug("Discovered a new writer {} for topic {}, type {}", key, writerData.getTopicName(), writerData.getTypeName());
 			}
-			else {
-				r.addMatchedWriter(writerData);
+
+			RTPSReader<?> r = participant.getReaderForTopic(writerData.getTopicName());
+			if (r != null) {
+				if (wdSample.isDisposed()) {
+					r.removeMatchedWriter(writerData);
+				}
+				else {
+					r.addMatchedWriter(writerData);
+				}
 			}
 		}
 	}
