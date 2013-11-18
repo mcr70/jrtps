d2049eaf776a7b338f06c02522dfc40c93b02df6
mcr70 <mcr70@localhost>
2013-11-18 16:17:32 +0200

    Added InconsistentPolicy

---
+++ jrtps/src/main/java/net/sf/jrtps/InconsistentPolicy.java
@@ -0,0 +1,16 @@
+package net.sf.jrtps;
+
+/**
+ * InconsistentPolicy exception. This exception gets thrown by setPolicy method,
+ * if the policy being set is somehow insconsistent.
+ * 
+ * @author mcr70
+ *
+ */
+public class InconsistentPolicy extends Exception {
+	private static final long serialVersionUID = 1L;
+
+	public InconsistentPolicy(String msg) {
+		super(msg);
+	}
+}
