204b2003b14c1ebc4275722755f6a5083740dec1
mcr70 <mcr70@localhost>
2013-11-19 11:38:36 +0200

    Updated javadocs

--- jrtps/src/main/java/net/sf/jrtps/types/Duration_t.java
+++ jrtps/src/main/java/net/sf/jrtps/types/Duration_t.java
@@ -2,26 +2,56 @@ package net.sf.jrtps.types;
 
 import net.sf.jrtps.transport.RTPSByteBuffer;
 
+/**
+ * Duration_t.
+ * 
+ * @author mcr70
+ *
+ */
 public class Duration_t {
+	// TODO: check infinite duration
 	public static final Duration_t INFINITE = new Duration_t(Integer.MAX_VALUE, 0);
 	public int sec;
 	public int nano;
 
+	/**
+	 * Constructor for Duration_t
+	 * @param millis Duration expressed in milliseconds.
+	 */
+	public Duration_t(int millis) {
+		this.sec = (int) (millis / 1000);
+		this.nano = 0;
+	}
+	
+	/**
+	 * Constructor for Duration_t
+	 * @param sec seconds
+	 * @param nano nanoseconds
+	 */
 	public Duration_t(int sec, int nano) {
 		this.sec = sec;
 		this.nano = nano;
 	}
 	
-	
+	/**
+	 * Constructs Duration_t from RTPSByteBuffer.
+	 * @param bb 
+	 */
 	public Duration_t(RTPSByteBuffer bb) {
 		sec = bb.read_long();
 		nano = bb.read_long();
 	}
 	
-	public String toString() {
-		return "[" + sec + ":" + nano + "]";
+	public void writeTo(RTPSByteBuffer buffer) {
+		buffer.write_long(sec);
+		buffer.write_long(nano);
 	}
 
+	/**
+	 * Gets this duration as milliseconds.
+	 * 
+	 * @return duration as milliseconds
+	 */
 	public long asMillis() {
 		long n = 0;
 		if (nano != 0) {
@@ -29,9 +59,8 @@ public class Duration_t {
 		}
 		return (long)sec * 1000 + n;
 	}
-	
-	public void writeTo(RTPSByteBuffer buffer) {
-		buffer.write_long(sec);
-		buffer.write_long(nano);
-	}
+
+	public String toString() {
+		return "[" + sec + ":" + nano + "]";
+	}	
 }
