93b137264e10b0cab6e33372d3a97ce869698bf4
mcr70 <mika.riekkinen@gmail.com>
2014-02-03 10:26:08 +0200

    Added initial version for opendds Messenger test

---
+++ udds/src/test/java/net/sf/jrtps/udds/opendds/messenger/MessageMarshaller.java
@@ -0,0 +1,53 @@
+package net.sf.jrtps.udds.opendds.messenger;
+
+import java.io.IOException;
+
+import net.sf.jrtps.Marshaller;
+import net.sf.jrtps.message.data.CDREncapsulation;
+import net.sf.jrtps.message.data.DataEncapsulation;
+import net.sf.jrtps.transport.RTPSByteBuffer;
+
+public class MessageMarshaller implements Marshaller<Message> {
+
+	@Override
+	public boolean hasKey() {
+		return true;
+	}
+
+	@Override
+	public byte[] extractKey(Message data) {
+		byte[] bytes = new byte[1];
+		bytes[0] = (byte) data.subject_id;
+		return bytes;
+	}
+
+	@Override
+	public Message unmarshall(DataEncapsulation dEnc) throws IOException {
+		CDREncapsulation cdrEnc = (CDREncapsulation) dEnc;		
+		RTPSByteBuffer bb = cdrEnc.getBuffer();
+
+		Message msg = new Message();
+		msg.from = bb.read_string();
+		msg.subject = bb.read_string();
+		msg.subject_id = bb.read_long();
+		msg.text = bb.read_string();
+		msg.count = bb.read_long();
+
+		return msg;
+	}
+
+	@Override
+	public DataEncapsulation marshall(Message msg) throws IOException {
+		CDREncapsulation cdrEnc = new CDREncapsulation(1024);
+		RTPSByteBuffer bb = cdrEnc.getBuffer();
+		
+		bb.write_string(msg.from);
+		bb.write_string(msg.subject);
+		bb.write_long(msg.subject_id);
+		bb.write_string(msg.text);
+		bb.write_long(msg.count);
+		
+		return cdrEnc;
+	}
+
+}
---
+++ udds/src/test/java/net/sf/jrtps/udds/opendds/messenger/Publisher.java
@@ -0,0 +1,22 @@
+package net.sf.jrtps.udds.opendds.messenger;
+
+import net.sf.jrtps.udds.DataWriter;
+import net.sf.jrtps.udds.Participant;
+
+public class Publisher {
+	public static void main(String[] args) throws Exception {
+		Participant p = new Participant(0, 2); // Create participant; domain 0, participant 2
+		p.setMarshaller(Message.class, new MessageMarshaller());
+		
+		DataWriter<Message> dw = p.createDataWriter("Movie Discussion List", 
+				Message.class, "Messenger::Message", new MessengerQoS());
+		
+		for (int i = 0; i < 10; i++) {
+			Message m = new Message("from foo" + i, "subject bar" + i, i, "text baz" + i, i);
+			dw.write(m);
+			Thread.sleep(1000);
+		}
+		
+		p.close();
+	}
+}
