2c5c231d5ca4027435a63c2883d83eb2f8a7e83d
mcr70 <mcr70@localhost>
2014-04-01 12:25:03 +0300

    Updated javadocs, DataWriter exposes Instances

--- src/main/java/net/sf/jrtps/message/parameter/KeyHash.java
+++ src/main/java/net/sf/jrtps/message/parameter/KeyHash.java
@@ -37,16 +37,13 @@ public class KeyHash extends Parameter implements InlineParameter {
 
     @Override
     public void read(RTPSByteBuffer bb, int length) {
-        readBytes(bb, length); // TODO: default reading. just reads to byte[] in
-                               // super class.
+        readBytes(bb, length); // TODO: default reading. just reads to byte[] in super class.
     }
 
     @Override
     public void writeTo(RTPSByteBuffer bb) {
-        writeBytes(bb); // TODO: default writing. just writes byte[] in super
-                        // class
+        writeBytes(bb); // TODO: default writing. just writes byte[] in super class
     }
-    
     
     public boolean equals(Object other) {
         if (other instanceof KeyHash) {
