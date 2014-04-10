a432917fe97d67140068e01b21fb0e9c5b580179
Mika Riekkinen <mika.riekkinen@gmail.com>
2014-04-10 10:22:37 +0300

    .

--- src/main/java/net/sf/jrtps/transport/UDPReceiver.java
+++ src/main/java/net/sf/jrtps/transport/UDPReceiver.java
@@ -4,12 +4,8 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
-import java.net.MulticastSocket;
-import java.net.SocketException;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.Semaphore;
-
-import net.sf.jrtps.types.Locator;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -28,41 +24,20 @@ public class UDPReceiver implements Runnable {
 
     private final Semaphore initLock = new Semaphore(1);
     private final BlockingQueue<byte[]> queue;
-    private final Locator locator;
-
+    private final DatagramSocket socket;
+    
     private boolean running = true;
-    DatagramSocket socket = null;
 
     private int bufferSize;
 
-    public UDPReceiver(Locator locator, BlockingQueue<byte[]> queue, int bufferSize) throws SocketException {
-        this.locator = locator;
+    public UDPReceiver(DatagramSocket ds, BlockingQueue<byte[]> queue, int bufferSize) {
+        this.socket = ds;
         this.queue = queue;
         this.bufferSize = bufferSize;
     }
 
     public void run() {
-        try {
-            initLock.acquire();
-        } catch (InterruptedException e1) {
-            log.debug("Failed to acquire lock duringin initialization. exiting.");
-            return;
-        }
-
-        try {
-            if (locator.getInetAddress().isMulticastAddress()) {
-                socket = new MulticastSocket(locator.getPort());
-                ((MulticastSocket) socket).joinGroup(locator.getInetAddress());
-            } else {
-                socket = new DatagramSocket(locator.getPort());
-            }
-        } catch (IOException ioe) {
-            log.warn("Got IOException during creation of socket", ioe);
-        }
-
-        log.debug("Listening on {}", locator);
-        initLock.release();
-
+        log.debug("Starting to listen on port {}", socket.getLocalPort());
         byte[] buf = new byte[bufferSize];
 
         while (running) {
@@ -72,33 +47,27 @@ public class UDPReceiver implements Runnable {
 
                 byte[] bytes = new byte[p.getLength()];
                 System.arraycopy(p.getData(), 0, bytes, 0, bytes.length);
-                log.debug("Received {} bytes from {}", bytes.length, locator);
+                log.debug("Received {} bytes from port {}", bytes.length, socket.getLocalPort());
 
                 queue.put(bytes);
             } catch (IOException se) {
                 // Ignore. If we are still running, try to receive again
             } catch (InterruptedException ie) {
+                Thread.currentThread().interrupt();
                 running = false;
             }
         } // while(...)
     }
 
     public void close() {
-        log.debug("Closing {}, {}", locator, socket);
-        try {
-            initLock.acquire();
-            if (socket != null) {
-                socket.close();
-            }
-            running = false;
-        } catch (InterruptedException e) {
-            log.debug("close() was interrupted");
+        log.debug("Closing {}", socket.getLocalPort());
+        
+        if (socket != null) {
+            socket.close();
         }
+        running = false;
     }
 
-    public Locator getLocator() {
-        return locator;
-    }
 
     @SuppressWarnings("unused")
     private void writeMessage(String string, byte[] msgBytes) {
