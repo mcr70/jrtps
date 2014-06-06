b6321d400a3e8710fd48e8f6e7112280aa154453
Mika Riekkinen <mika.riekkinen@gmail.com>
2014-06-06 15:29:24 +0300

    Added missing methods for coherent set

--- src/main/java/net/sf/jrtps/message/parameter/CoherentSet.java
+++ src/main/java/net/sf/jrtps/message/parameter/CoherentSet.java
@@ -1,19 +1,30 @@
 package net.sf.jrtps.message.parameter;
 
 import net.sf.jrtps.transport.RTPSByteBuffer;
+import net.sf.jrtps.types.SequenceNumber;
 
 public class CoherentSet extends Parameter implements InlineParameter {
+    private SequenceNumber seqNum;
+    
     CoherentSet() {
         super(ParameterEnum.PID_COHERENT_SET);
     }
 
+    /**
+     * Gets the sequence number of the first sample in coherent set.
+     * @return sequence number
+     */
+    public SequenceNumber getStartSeqNum() {
+        return seqNum;
+    }
+    
     @Override
     public void read(RTPSByteBuffer bb, int length) {
-        readBytes(bb, length); 
+        this.seqNum = new SequenceNumber(bb); 
     }
 
     @Override
     public void writeTo(RTPSByteBuffer bb) {
-        writeBytes(bb);
+        seqNum.writeTo(bb);
     }
 }
