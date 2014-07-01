9118e7c78debfdcbc7561edcdd516d2f8d792f4a
Mika Riekkinen <mika.riekkinen@gmail.com>
2014-07-01 07:51:06 +0300

    Changed List<Guid> to Guid[]

--- src/main/java/net/sf/jrtps/message/parameter/DirectedWrite.java
+++ src/main/java/net/sf/jrtps/message/parameter/DirectedWrite.java
@@ -1,43 +1,40 @@
 package net.sf.jrtps.message.parameter;
 
-import java.util.LinkedList;
-import java.util.List;
-
 import net.sf.jrtps.transport.RTPSByteBuffer;
 import net.sf.jrtps.types.Guid;
 
 public class DirectedWrite extends Parameter implements InlineParameter {
-    private List<Guid> guids;
+    private Guid[] guids;
 
     DirectedWrite() {
         super(ParameterEnum.PID_DIRECTED_WRITE);
     }
 
-    public DirectedWrite(List<Guid> guids) {
+    public DirectedWrite(Guid[] guids) {
         super(ParameterEnum.PID_DIRECTED_WRITE);
         this.guids = guids;        
     }
     
     /**
      * Gets the guids of this DirectedWriter
-     * @return a List of Guids
+     * @return an array of Guids
      */
-    public List<Guid> getGuids() {
+    public Guid[] getGuids() {
         return guids;
     }
     
     @Override
     public void read(RTPSByteBuffer bb, int length) {
-        guids = new LinkedList<>();
         int size = bb.read_long();
+        this.guids = new Guid[size];
         for (int i = 0; i < size; i++) {
-            guids.add(new Guid(bb));
+            guids[i] = new Guid(bb);
         }
     }
 
     @Override
     public void writeTo(RTPSByteBuffer bb) {
-        bb.write_long(guids.size());
+        bb.write_long(guids.length);
         for (Guid guid : guids) {
             guid.writeTo(bb);
         }
