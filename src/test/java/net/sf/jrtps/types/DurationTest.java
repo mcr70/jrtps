9430d5ec28c14164c1296ce3daf40996ff65b89c
Mika Riekkinen <mika.riekkinen@gmail.com>
2014-09-25 09:33:32 +0300

    FIX: Duration nsec calculation from millis

---
+++ src/test/java/net/sf/jrtps/types/DurationTest.java
@@ -0,0 +1,28 @@
+package net.sf.jrtps.types;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class DurationTest {
+    @Test
+    public void testMillisConstructor() {
+        Duration d1 = new Duration(1000);
+        assertEquals(1, d1.getSeconds());
+        assertEquals(0, d1.getNanoSeconds());
+        
+        Duration d2 = new Duration(1);
+        assertEquals(0, d2.getSeconds());
+        assertEquals(1000000, d2.getNanoSeconds());
+    }
+
+
+    @Test
+    public void testAsMillis() {
+        Duration d1 = new Duration(1000);
+        assertEquals(1000, d1.asMillis());
+
+        Duration d2 = new Duration(1, 1000000);
+        assertEquals(1001, d2.asMillis());
+    }
+}
