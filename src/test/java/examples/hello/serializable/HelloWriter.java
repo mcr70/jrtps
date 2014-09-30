837d2fc20b2194f6cb082b086ddb6b09517fedad
mcr70 <mcr70@localhost>
2014-09-30 09:45:58 +0300

    PARTITION QoS policy

--- src/test/java/examples/hello/serializable/HelloWriter.java
+++ src/test/java/examples/hello/serializable/HelloWriter.java
@@ -2,6 +2,8 @@ package examples.hello.serializable;
 
 import java.net.SocketException;
 
+import net.sf.jrtps.QualityOfService;
+import net.sf.jrtps.message.parameter.QosPartition;
 import net.sf.jrtps.udds.DataWriter;
 import net.sf.jrtps.udds.Participant;
 
@@ -9,8 +11,11 @@ public class HelloWriter {
     public static void main(String[] args) throws InstantiationException, IllegalAccessException, SocketException,
             InterruptedException {
         Participant p = new Participant(0); // domain 0, participant 2
-
-        DataWriter<HelloMessage> dw = p.createDataWriter(HelloMessage.class);
+        
+        QualityOfService qos = new QualityOfService();
+        qos.setPolicy(new QosPartition(new String[]{"partition-1"}));
+        
+        DataWriter<HelloMessage> dw = p.createDataWriter(HelloMessage.class, qos);
         
         for (int i = 0; i < 10; i++) {
             HelloMessage m = new HelloMessage(i , "Hello");
