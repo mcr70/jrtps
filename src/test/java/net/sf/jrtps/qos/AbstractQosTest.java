7859c9bb40faf4cd01e9ddd1cd779b74acc66862
Mika Riekkinen <mika.riekkinen@gmail.com>
2014-10-15 11:23:26 +0300

    Added OwnershipTest

---
+++ src/test/java/net/sf/jrtps/qos/AbstractQosTest.java
@@ -0,0 +1,83 @@
+package net.sf.jrtps.qos;
+
+import java.util.concurrent.CountDownLatch;
+
+import net.sf.jrtps.Configuration;
+import net.sf.jrtps.builtin.PublicationData;
+import net.sf.jrtps.builtin.SubscriptionData;
+import net.sf.jrtps.message.parameter.KeyHash;
+import net.sf.jrtps.transport.TransportProvider;
+import net.sf.jrtps.transport.mem.MemProvider;
+import net.sf.jrtps.udds.CommunicationListener;
+import net.sf.jrtps.udds.DataReader;
+import net.sf.jrtps.udds.DataWriter;
+import net.sf.jrtps.udds.Participant;
+
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+
+import examples.hello.serializable.HelloMessage;
+
+public class AbstractQosTest {
+    static final long LATCH_WAIT_SECS = 2;
+
+    static Configuration cfg1 = new Configuration("/mem-test-1.properties");
+    static Configuration cfg2 = new Configuration("/mem-test-2.properties");
+
+    static {
+        MemProvider mp = new MemProvider(cfg1);
+        TransportProvider.registerTransportProvider("mem", mp, MemProvider.LOCATOR_KIND_MEM);
+    }
+
+    Participant p1;
+    Participant p2;
+
+    @Before
+    public void init() {
+        // Create two participants; one reader, one for writer
+        p1 = new Participant(0,0, null, cfg1);
+        p2 = new Participant(0,0, null, cfg2);
+    }
+
+    @After 
+    public void destroy() {
+        p1.close();        
+        p2.close();
+    }
+
+
+    void addCommunicationListener(DataReader<HelloMessage> dr, final CountDownLatch dlLatch, final CountDownLatch emLatch) {
+        dr.addCommunicationListener(new CommunicationListener<PublicationData>() {            
+            @Override
+            public void deadlineMissed(KeyHash instanceKey) {
+                dlLatch.countDown();
+            }
+            @Override
+            public void inconsistentQoS(PublicationData ed) {
+                Assert.fail("Inconsistent QoS not expected");
+            }            
+            @Override
+            public void entityMatched(PublicationData ed) {
+                emLatch.countDown();
+            }
+        });    }
+
+    void addCommunicationListener(DataWriter<HelloMessage> dw, final CountDownLatch dlLatch, final CountDownLatch emLatch) {
+        dw.addCommunicationListener(new CommunicationListener<SubscriptionData>() {            
+            @Override
+            public void deadlineMissed(KeyHash instanceKey) {
+                dlLatch.countDown();
+            }
+
+            @Override
+            public void inconsistentQoS(SubscriptionData ed) {
+                Assert.fail("Inconsistent QoS not expected");
+            }
+            @Override
+            public void entityMatched(SubscriptionData ed) {
+                emLatch.countDown();
+            }
+        });
+    }
+}
---
+++ src/test/java/net/sf/jrtps/qos/HistoryTest.java
@@ -0,0 +1,82 @@
+package net.sf.jrtps.qos;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import java.util.List;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+
+import net.sf.jrtps.QualityOfService;
+import net.sf.jrtps.message.parameter.QosDurability;
+import net.sf.jrtps.message.parameter.QosDurability.Kind;
+import net.sf.jrtps.message.parameter.QosHistory;
+import net.sf.jrtps.rtps.Sample;
+import net.sf.jrtps.udds.DataReader;
+import net.sf.jrtps.udds.DataWriter;
+import net.sf.jrtps.udds.SampleListener;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import examples.hello.serializable.HelloMessage;
+
+
+public class HistoryTest extends AbstractQosTest {
+
+    /**
+     * Test for HISTORY QoS policy.
+     *  1.  Create TRANSIENT_LOCAL reader TRANSIENT_LOCAL writer with HISTORY(2) 
+     *  2.  write 3 samples to same instance.
+     *  3.  wait for entities to be matched
+     *  4.  wait for TRANSIENT_LOCAL reader to receive _3_ samples.
+     *      If timeout occurs --> SUCCESS
+     */
+    @Test
+    public void testHistory() {
+        QualityOfService qos= new QualityOfService();
+        qos.setPolicy(new QosDurability(Kind.TRANSIENT_LOCAL));
+        qos.setPolicy(new QosHistory(QosHistory.Kind.KEEP_LAST, 2));
+
+        // Create DataWriter and write some samples
+        DataWriter<HelloMessage> dw = p2.createDataWriter(HelloMessage.class, qos);
+
+        dw.write(new HelloMessage(1, "hello"));
+        dw.write(new HelloMessage(1, "hello"));
+        dw.write(new HelloMessage(1, "hello"));
+
+        // Create DataReaders
+        DataReader<HelloMessage> drTRLocal = p1.createDataReader(HelloMessage.class, qos);
+
+        // Latch used to synchronize on entity matched
+        final CountDownLatch emLatch = new CountDownLatch(2); 
+        final CountDownLatch trDataLatch = new CountDownLatch(3);
+        drTRLocal.addSampleListener(new SampleListener<HelloMessage>() {
+            @Override
+            public void onSamples(List<Sample<HelloMessage>> samples) {
+                for (int i = 0; i < samples.size(); i++) {
+                    trDataLatch.countDown();
+                }
+            }
+        });
+
+        addCommunicationListener(drTRLocal, null, emLatch);
+        addCommunicationListener(dw, null, emLatch);
+
+        // Wait for the readers and writer to be matched
+        try {
+            boolean await = emLatch.await(LATCH_WAIT_SECS, TimeUnit.SECONDS);
+            assertTrue("Entities were not matched in time", await);
+        } catch (InterruptedException e) {
+            Assert.fail("Interrupted");
+        }
+
+        // Wait for transient local reader to receive all the samples
+        try {
+            boolean await = trDataLatch.await(1000, TimeUnit.MILLISECONDS); 
+            assertFalse("Received more than 2 samples" + trDataLatch.getCount(), await);
+        } catch (InterruptedException e) {
+            Assert.fail("Interrupted");
+        }
+    }
+}
