0f96e28a6e05ab0a199c0b58b3484cf0683797e3
Mika Riekkinen <mika.riekkinen@gmail.com>
2014-11-14 11:01:55 +0200

    removed ----BEGIN CERTIFICATE---- & ----END CERTIFICATE----

--- src/test/java/net/sf/jrtps/udds/security/AuthenticationTest.java
+++ src/test/java/net/sf/jrtps/udds/security/AuthenticationTest.java
@@ -1,5 +1,6 @@
 package net.sf.jrtps.udds.security;
 
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.security.InvalidKeyException;
 import java.security.KeyStoreException;
@@ -8,6 +9,10 @@ import java.security.NoSuchProviderException;
 import java.security.SignatureException;
 import java.security.UnrecoverableKeyException;
 import java.security.cert.CertificateException;
+import java.security.cert.CertificateFactory;
+import java.security.cert.X509Certificate;
+
+import javax.xml.bind.DatatypeConverter;
 
 import net.sf.jrtps.qos.AbstractQosTest;
 import net.sf.jrtps.types.EntityId;
@@ -26,6 +31,14 @@ public class AuthenticationTest extends AbstractQosTest {
 		System.out.println(ks.getLocalIdentity().getIdentityToken());
 		System.out.println(ks.getLocalIdentity().getIdentityCreadential());
 		
-		ks.beginHandshakeRequest(null);
+		CertificateFactory cf = CertificateFactory.getInstance("X.509");
+		String pem = ks.getLocalIdentity().getIdentityCreadential().getPEMEncodedCertificate();
+		byte[] bytes = DatatypeConverter.parseBase64Binary(pem);
+		
+		X509Certificate certificate = (X509Certificate) cf.generateCertificate(new ByteArrayInputStream(bytes));
+		System.out.println("Got certificate from PEM: " + certificate.getSubjectDN());
+		
+		
+		//ks.beginHandshakeRequest(null);
 	}
 }
