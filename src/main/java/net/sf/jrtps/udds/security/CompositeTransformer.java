948df73ee90a1281bec567e6b3e70d69b1bfc26d
mcr70 <mcr70@localhost>
2015-01-10 16:42:44 +0200

    Updated javadoc

--- src/main/java/net/sf/jrtps/udds/security/CompositeTransformer.java
+++ src/main/java/net/sf/jrtps/udds/security/CompositeTransformer.java
@@ -12,16 +12,25 @@ import net.sf.jrtps.transport.RTPSByteBuffer;
  * @author mcr70
  */
 class CompositeTransformer implements Transformer {
+	public static final String AES_HMAC_SHA1_NAME = "aes_hmac_sha1";
+    public static final String AES_HMAC_SHA256_NAME = "aes_hmac_sha256";
+    public static final int AES_HMAC_SHA1 = 0xff000200;
+    public static final int AES_HMAC_SHA256 = 0xff000201;
+	
 	private final Transformer tr1;
 	private final int kind;
 	private final String name;
 	private final Transformer tr2;
-
-	public CompositeTransformer(String name, int kind, Transformer tr1, Transformer tr2) {
-		this.name = name;
+	
+	public CompositeTransformer(int kind, Transformer tr1, Transformer tr2) {
+		this.name = tr1.getName() + "+" + tr2.getName();
 		this.kind = kind;
 		this.tr1 = tr1;
 		this.tr2 = tr2;
+		
+		if (tr1 == null || tr2 == null) {
+			throw new IllegalArgumentException("Transformer cannot be null");
+		}
 	}
 
 	@Override
@@ -29,6 +38,11 @@ class CompositeTransformer implements Transformer {
 		return kind;
 	}
 
+	/**
+	 * Name of this CompositeTransformer. Name is composed of the names of 
+	 * Transformers, so that name is tr1.name+tr2.name. For example, "AES+HmacSHA1"  
+	 * @return name
+	 */
 	@Override
 	public String getName() {
 		return name;
@@ -44,9 +58,9 @@ class CompositeTransformer implements Transformer {
 
 	@Override
 	public RTPSByteBuffer decode(Key key, SecurePayload payload) {
-		RTPSByteBuffer decode = tr2.decode(key, payload);
-		tr1.decode(key, null); // TODO: read SecurePayload from RTPSByteBuffer
+		RTPSByteBuffer bb1 = tr2.decode(key, payload);
+		RTPSByteBuffer bb2 = tr1.decode(key, new SecurePayload(bb1)); 
 		
-		return null;
+		return bb2;
 	}
 }
--- src/main/java/net/sf/jrtps/udds/security/SecurePayload.java
+++ src/main/java/net/sf/jrtps/udds/security/SecurePayload.java
@@ -2,11 +2,29 @@ package net.sf.jrtps.udds.security;
 
 import net.sf.jrtps.transport.RTPSByteBuffer;
 
+/**
+ * SecurePayload.
+ * 
+ * @author mcr70
+ */
 public class SecurePayload {
 	private final int transformationKind;  // long, one of predefined integers above
 	private final byte[] transformationId; // octet[8]
 	private final byte[] cipherText;       // octet[*]
 
+	/**
+	 * Reads SecurePayload from given RTPSByteBuffer
+	 * @param bb
+	 */
+	SecurePayload(RTPSByteBuffer bb) {
+		this.transformationKind = bb.read_long();
+		this.transformationId = new byte[8];
+		bb.read(transformationId);
+		
+		this.cipherText = new byte[bb.read_long()];
+		bb.read(cipherText);
+	}
+	
 	public SecurePayload(int transformationKind, byte[] cipherText) {
 		this(transformationKind, new byte[8], cipherText);
 	}
@@ -25,18 +43,34 @@ public class SecurePayload {
         }
 	}
 	
+	/**
+	 * Gets transformation kind
+	 * @return transformationKind
+	 */
 	public int getTransformationKind() {
 		return transformationKind;
 	}
 	
+	/**
+	 * Gets transformationId. Transformation id is a byte array of length 8.
+	 * @return transformationId
+	 */
 	public byte[] getTrasformationId() {
 		return transformationId;
 	}
 	
+	/**
+	 * Gets the cipher text
+	 * @return cipherText
+	 */
 	public byte[] getCipherText() {
 		return cipherText;
 	}
 
+	/**
+	 * Writes this SecurePayload to given RTPSByteBuffer
+	 * @param bb
+	 */
 	public void writeTo(RTPSByteBuffer bb) {
         bb.write_long(transformationKind);
         bb.write(transformationId);
