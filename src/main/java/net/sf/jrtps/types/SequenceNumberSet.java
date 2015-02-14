1f5c898aaac393cb0d47b83c3129e2b5d5d9eef2
mcr70 <mcr70@localhost>
2015-02-14 09:14:35 +0200

    Added test for missing sequence numbers in SequenceNumberSet

--- src/main/java/net/sf/jrtps/types/SequenceNumberSet.java
+++ src/main/java/net/sf/jrtps/types/SequenceNumberSet.java
@@ -6,22 +6,36 @@ import java.util.List;
 import net.sf.jrtps.transport.RTPSByteBuffer;
 
 /**
+ * SequenceNumberSet contains a number of sequence numbers encoded in a bitmap.
+ * MSB bit in bitmap represents base of the SequenceNumberSet.
+ * 
  * see 9.4.2.6 SequenceNumberSet
  * 
  * @author mcr70
- * 
  */
 public class SequenceNumberSet {
     private final SequenceNumber bitmapBase;
     private final int[] bitmaps;
     private final int numBits;
+	private List<Long> seqNums;
+	private List<Long> missingSeqNums;
 
+    /**
+     * Constructor for SequenceNumberSet. 
+     * 
+     * @param base base of the SequenceNumberSet
+     * @param bitmaps Bitmap of the contained sequence numbers in set
+     */
     public SequenceNumberSet(long base, int[] bitmaps) {
         this.bitmapBase = new SequenceNumber(base);
         this.bitmaps = bitmaps;
         this.numBits = bitmaps.length * 32;
     }
 
+    /**
+     * Reads a SequenceNumberSet from RTPSByteBuffer.
+     * @param bb
+     */
     public SequenceNumberSet(RTPSByteBuffer bb) {
         bitmapBase = new SequenceNumber(bb);
 
@@ -39,7 +53,7 @@ public class SequenceNumberSet {
      * @param seqNum
      * @return true or false
      */
-    public boolean isSet(long seqNum) {
+    public boolean containsSeqNum(long seqNum) {
     	return getSequenceNumbers().contains(seqNum);
     }
     
@@ -75,24 +89,12 @@ public class SequenceNumberSet {
      * @return a List of sequence numbers
      */
     public List<Long> getSequenceNumbers() {
-        List<Long> seqNums = new LinkedList<Long>();
-
-        long seqNum = bitmapBase.getAsLong();
-        int bitCount = 0;
-
-        for (int i = 0; i < bitmaps.length; i++) {
-            int bitmap = bitmaps[i];
-
-            for (int j = 0; j < 32 && bitCount < numBits; j++) {
-                if ((bitmap & 0x80000000) == 0x80000000) { // id the MSB matches, add a new seqnum
-                    seqNums.add(seqNum);
-                }
-
-                seqNum++; bitCount++;
-                bitmap = bitmap << 1;
-            }
-        }
-
+    	if (seqNums != null) {
+    		return seqNums;
+    	}
+    	
+        seqNums = createSeqNumList(0x80000000);
+        
         return seqNums;
     }
 
@@ -101,25 +103,35 @@ public class SequenceNumberSet {
      * @return a List of missing sequence numbers
      */
     public List<Long> getMissingSequenceNumbers() {
-        List<Long> seqNums = new LinkedList<Long>();
+        if (missingSeqNums != null) {
+        	return missingSeqNums;
+        }
+
+        missingSeqNums = createSeqNumList(0x0);
+        
+        return missingSeqNums;
+    }
+    
+    private List<Long> createSeqNumList(int msbValue) {
+    	List<Long> snList = new LinkedList<Long>();
 
         long seqNum = bitmapBase.getAsLong();
-
+        int bitCount = 0;
+        
         for (int i = 0; i < bitmaps.length; i++) {
             int bitmap = bitmaps[i];
 
-            for (int j = 0; j < 32; j++) {
-                if ((bitmap & 0x8000000) == 0x0) { // id the MSB does not
-                                                   // matches, add a new seqnum
-                    seqNums.add(seqNum);
+            for (int j = 0; j < 32 && bitCount < numBits; j++) {
+                if ((bitmap & 0x80000000) == msbValue) { // Compare MSB to 0x80000000 or 0x0
+                    snList.add(seqNum);
                 }
 
-                seqNum++;
+                seqNum++; bitCount++;
                 bitmap = bitmap << 1;
             }
         }
 
-        return seqNums;
+        return snList;
     }
 
     public void writeTo(RTPSByteBuffer buffer) {
