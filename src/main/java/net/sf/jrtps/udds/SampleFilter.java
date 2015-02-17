06476f464b30bb5a6789888f711105f48366f768
mcr70 <mcr70@localhost>
2015-02-17 07:39:29 +0200

    SampleFilter

--- src/main/java/net/sf/jrtps/udds/SampleFilter.java
+++ src/main/java/net/sf/jrtps/udds/SampleFilter.java
@@ -3,11 +3,19 @@ package net.sf.jrtps.udds;
 import net.sf.jrtps.rtps.Sample;
 
 /**
- * A Filter interface is used for reader side filtering.
+ * A SampleFilter is used to filter incoming samples at reader.
+ * 
  * @author mcr70
  *
  * @param <T>
  */
 interface SampleFilter<T> {
+	/**
+	 * This method is called to determine whether or not an incoming
+	 * Sample should be added to readers history cache.
+	 * 
+	 * @param sample incoming Sample
+	 * @return true or false
+	 */
     boolean acceptSample(Sample<T> sample);
 }
---
+++ src/test/java/net/sf/jrtps/udds/SampleFilterTest.java
@@ -0,0 +1,5 @@
+package net.sf.jrtps.udds;
+
+public class SampleFilterTest {
+
+}
