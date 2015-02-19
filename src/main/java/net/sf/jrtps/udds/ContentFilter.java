e88e7739a4303c75e4d9a7a5d04d1fd0cee52146
Mika Riekkinen <mika.riekkinen@gmail.com>
2015-02-19 07:39:53 +0200

    Removed SampleFilter

--- src/main/java/net/sf/jrtps/udds/ContentFilter.java
+++ src/main/java/net/sf/jrtps/udds/ContentFilter.java
@@ -1,16 +1,27 @@
 package net.sf.jrtps.udds;
 
 import net.sf.jrtps.message.parameter.ContentFilterProperty;
+import net.sf.jrtps.rtps.Sample;
 
 /**
- * ContentFilter interface provides ContentFilterProperty which is transferred 
- * to writers.
+ * ContentFilter.
  * 
  * @author mcr70
  *
  * @param <T>
  */
-public interface ContentFilter<T> extends SampleFilter<T> {
+public interface ContentFilter<T> {
+	/**
+	 * This method is called to determine whether or not a
+	 * Sample should be passed forward. On reader side, this means that
+	 * Sample is added to readers history cache. On writer side,
+	 * this determines whether or not to send Sample to reader.
+	 * 
+	 * @param sample Sample
+	 * @return true or false
+	 */
+    boolean acceptSample(Sample<T> sample);
+
 	/**
 	 * Gets a ContentFilterProperty that will be sent to writers
 	 * @return ContentFilterProperty
