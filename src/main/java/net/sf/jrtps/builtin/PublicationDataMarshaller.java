2267b6ac8314976877f4576d2d93326d6287c421
mcr70 <mcr70@localhost>
2015-02-26 08:44:48 +0200

    hasKey() returns false. This is a workaround for interoperability with RTI Connext.

--- src/main/java/net/sf/jrtps/builtin/PublicationDataMarshaller.java
+++ src/main/java/net/sf/jrtps/builtin/PublicationDataMarshaller.java
@@ -34,7 +34,9 @@ public class PublicationDataMarshaller implements Marshaller<PublicationData> {
      */
     @Override
     public boolean hasKey() {
-        return true;
+    	// Always false, KeyHash is optional and RTI Connext expects
+    	// that KeyHash of builtin topics is not a MD5 sum
+    	return false; // Always true. Key is PID_KEY_HASH.
     }
 
     /**
