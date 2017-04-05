3b5623b987a78685c9c4e0a42fe5ceb008a2ef6c
mcr70 <mika.riekkinen@gmail.com>
2017-04-05 08:22:46 +0300

    Request marshalling

--- dds-rpc/src/main/java/net/sf/jrtps/rpc/RequestMarshaller.java
+++ dds-rpc/src/main/java/net/sf/jrtps/rpc/RequestMarshaller.java
@@ -3,9 +3,12 @@ package net.sf.jrtps.rpc;
 import java.io.IOException;
 
 import net.sf.jrtps.Marshaller;
+import net.sf.jrtps.message.CDREncapsulation;
 import net.sf.jrtps.message.DataEncapsulation;
+import net.sf.jrtps.transport.RTPSByteBuffer;
 
 class RequestMarshaller implements Marshaller<Request> {
+   private final int bufferSize = 1024; // TODO: configurable
    private final Service service;
 
    public RequestMarshaller(Service service) {
@@ -24,13 +27,19 @@ class RequestMarshaller implements Marshaller<Request> {
 
    @Override
    public Request unmarshall(DataEncapsulation dEnc) throws IOException {
-      // TODO Auto-generated method stub
-      return null;
+      CDREncapsulation cdrEnc = (CDREncapsulation) dEnc;
+      RTPSByteBuffer bb = cdrEnc.getBuffer();
+
+      return new Request(bb);
    }
 
    @Override
    public DataEncapsulation marshall(Request data) throws IOException {
-      // TODO Auto-generated method stub
-      return null;
+      CDREncapsulation cdrEnc = new CDREncapsulation(bufferSize);
+      RTPSByteBuffer bb = cdrEnc.getBuffer();
+
+      data.writeTo(bb);
+      
+      return cdrEnc;
    }
 }
