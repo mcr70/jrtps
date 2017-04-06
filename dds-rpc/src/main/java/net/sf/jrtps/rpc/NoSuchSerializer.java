50e2cebd2cc65a21e16635b6dfc2d9c676a05d28
mcr70 <mika.riekkinen@gmail.com>
2017-04-06 09:05:45 +0300

    Some cleanup

--- dds-rpc/src/main/java/net/sf/jrtps/rpc/NoSuchSerializer.java
+++ dds-rpc/src/main/java/net/sf/jrtps/rpc/NoSuchSerializer.java
@@ -1,9 +1,17 @@
 package net.sf.jrtps.rpc;
 
+/**
+ * NoSuchSerializer exception. 
+ * @author mcr70
+ */
 public class NoSuchSerializer extends Exception {
    private static final long serialVersionUID = 1L;
 
-   public NoSuchSerializer(Class<?> returnType) {
-      super("No such Serializer: " + returnType.getName());
+   /**
+    * NoSuchSerializer
+    * @param type Type
+    */
+   public NoSuchSerializer(Class<?> type) {
+      super("No such Serializer: " + type.getName());
    }
 }
