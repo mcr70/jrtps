e8d24857456d7dd29210c3bc6ccd66f07b7073bc
Mika Riekkinen <mika.riekkinen@gmail.com>
2017-04-09 15:05:11 +0300

    Turned jrtps into multimodule build

--- dds-rpc/src/main/java/net/sf/jrtps/rpc/UnknownOperationException.java
+++
@@ -1,9 +0,0 @@
-package net.sf.jrtps.rpc;
-
-public class UnknownOperationException extends Exception {
-   private static final long serialVersionUID = 1L;
-
-   public UnknownOperationException(String message) {
-      super(message);
-   }
-}
