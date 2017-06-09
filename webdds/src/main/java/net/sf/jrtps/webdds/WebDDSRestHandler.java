8fa3e32dac730946b91d8096eb77da3c23045be4
mcr70 <mcr70@localhost>
2017-06-09 21:53:12 +0300

    Initial tomcat7 setup

--- webdds/src/main/java/net/sf/jrtps/webdds/WebDDSRestHandler.java
+++ webdds/src/main/java/net/sf/jrtps/webdds/WebDDSRestHandler.java
@@ -1,33 +1,41 @@
 package net.sf.jrtps.webdds;
 
+import java.util.LinkedList;
+import java.util.List;
+
 import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 
+@Path("/dds")
 public class WebDDSRestHandler {
-    
-    @DELETE
-    @Path("/applications")
-    void deleteApplication(String appName) {
-	throw new RuntimeException("Not supported");
-    }
 
-    @POST
-    @Path("/applications")
-    void createApplication(String application) {
-	throw new RuntimeException("Not supported");
-    }
+	@GET
+	@Path("/{param}")
+	public List<String> getApplications(@PathParam("param") String msg) {
+		List<String> l = new LinkedList<>();
+		l.add(msg);
+		return l;
+	}
 
-    @GET
-    @Path("/applications")
-    void getApplications() {
-	throw new RuntimeException("Not supported");	
-    }
-    
-    @GET
-    @Path("/types")
-    void getTypes() {
-	throw new RuntimeException("Not supported");
-    }
+	@DELETE
+	@Path("/applications")
+	public void deleteApplication(String appName) {
+		throw new RuntimeException("Not supported");
+	}
+
+	@POST
+	@Path("/applications")
+	public void createApplication(String application) {
+		throw new RuntimeException("Not supported");
+	}
+
+
+	@GET
+	@Path("/types")
+	public List<String> getTypes() {
+		throw new RuntimeException("Not supported");
+	}
 }
